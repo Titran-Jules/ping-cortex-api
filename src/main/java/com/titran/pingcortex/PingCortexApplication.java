37681022770be01c0b0ac48cba8463a770ebdad1
JulesTitran <titranj@gmail.com>
2026-09-02 14:40:43 +0300

    chore(config): add pom.xml dependecies needed

---
+++ src/main/java/com/titran/pingcortex/PingCortexApplication.java
@@ -0,0 +1,13 @@
+package com.titran.pingcortex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingCortexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingCortexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/titran/pingcortex/PingCortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titran.pingcortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingCortexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
