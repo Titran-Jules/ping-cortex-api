65af01cf8ac279a163ab9ed28d104a4e827c648d
JulesTitran <titranj@gmail.com>
2026-09-12 15:35:34 +0300

    feat(service): add course materials service with course existence verification

---
+++ src/main/java/com/titran/pingcortex/service/CourseMaterialService.java
@@ -0,0 +1,28 @@
+package com.titran.pingcortex.service;
+
+import com.titran.pingcortex.dto.request.CourseMaterialRequest;
+import com.titran.pingcortex.dto.response.CourseMaterialResponse;
+import com.titran.pingcortex.exception.CourseNotFoundException;
+import com.titran.pingcortex.repository.CourseMaterialRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class CourseMaterialService {
+    private final CourseMaterialRepository courseMaterialRepository;
+    private final CourseService courseService;
+
+    public List<CourseMaterialResponse> findAll(UUID userId, UUID courseId) {
+        courseService.findCourseById(userId, courseId).orElseThrow(CourseNotFoundException::new);
+        return courseMaterialRepository.findAllByCourseId(courseId);
+    }
+
+    public CourseMaterialResponse create(UUID userId, UUID courseId, CourseMaterialRequest courseMaterialRequest) {
+        courseService.findCourseById(userId, courseId).orElseThrow(CourseNotFoundException::new);
+        return courseMaterialRepository.create(courseId, courseMaterialRequest.content(), courseMaterialRequest.type());
+    }
+}
