37cfdb2c95b2edad4cabe4f9f9e76908f9b0d927
JulesTitran <titranj@gmail.com>
2026-09-19 14:04:13 +0300

    feat(config): implement AsyncConfigurer for Async

--- src/main/java/com/titran/pingcortex/config/AsyncConfig.java
+++ src/main/java/com/titran/pingcortex/config/AsyncConfig.java
@@ -1,15 +1,20 @@
 package com.titran.pingcortex.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.Arrays;
 import java.util.concurrent.Executor;
 
+@Slf4j
 @Configuration
 @EnableAsync
-public class AsyncConfig {
+public class AsyncConfig implements AsyncConfigurer {
     @Bean(name = "taskExecutor")
     public Executor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
@@ -19,5 +24,18 @@ public class AsyncConfig {
         executor.setThreadNamePrefix("ai-analysis-");
         executor.initialize();
         return executor;
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        return taskExecutor();
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) -> {
+            log.error("Exception occurred while executing async task: method={}, params={}",
+                    method.getName(), Arrays.toString(params), ex);
+        };
     }
 }
